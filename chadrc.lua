5d66556911ff1d9c9a8dac7010d42fad26661a59
Pham Hong Vinh <phamvinh257@gmail.com>
2022-08-24 01:35:10 +0700

    update new config

--- chadrc.lua
+++ chadrc.lua
@@ -5,22 +5,14 @@ local M = {}
 -- make sure you maintain the structure of `core/default_config.lua` here,
 -- example of changing theme:
 
-local customPlugins = require "custom.plugins"
+M.mappings = require "custom.mappings"
 
 M.plugins = {
-  default_plugin_remove = {
-    "rafamadriz/friendly-snippets",
-  },
-  install = customPlugins,
-  options = {
-    lspconfig = {
-      setup_lspconf = "custom.plugins.lspconfig",
-    }
-  }
+   user = require "custom.plugins"
 }
 
 M.ui = {
-  theme = "onedark",
+  theme = "gruvbox",
 }
 
 return M
--- plugins/init.lua
+++ plugins/init.lua
@@ -1,6 +1,62 @@
 return {
-   {
-      "rafamadriz/friendly-snippets",
-      --module = "cmp_nvim_lsp",
+   ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+   },
+
+   ["goolord/alpha-nvim"] = {
+      disable = false,
+   },
+
+   ["elkowar/yuck.vim"] = { ft = "yuck" },
+
+   ["max397574/better-escape.nvim"] = {
+      event = "InsertEnter",
+      config = function()
+         require("better_escape").setup()
+      end,
+   },
+
+   ["jose-elias-alvarez/null-ls.nvim"] = {
+      after = "nvim-lspconfig",
+      config = function()
+         require "custom.plugins.null-ls"
+      end,
+   },
+
+   ["williamboman/mason.nvim"] = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "json-lsp",
+        "yaml-language-server",
+
+        -- cpp dev
+        "clang-format",
+        "clangd",
+        "cpplint",
+
+        -- python dev
+        "isort",
+        "black",
+        "flake8",
+        "mypy",
+        "pyright",
+        "pylint",
+        "python-lsp-server",
+
+        -- shell
+        "shfmt",
+        "shellcheck",
+        "dockerfile-language-server",
+      },
    },
 }
